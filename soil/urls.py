96b9f8e21fc617c206551c09e0abf705a3f700e6
bahromjon begmuhammadov <hopebegmuhammadov@gmail.com>
2021-07-17 21:46:47 +0500

    added multilinguality

--- soil/urls.py
+++ soil/urls.py
@@ -14,19 +14,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
-from django.conf.urls.i18n import i18n_patterns
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
 
-urlpatterns = i18n_patterns(
-    url(r'^i18n/', include('django.conf.urls.i18n')),
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/main/', include('main.urls')),
     path('api/v1/', include('user.urls')),
-)
+]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
