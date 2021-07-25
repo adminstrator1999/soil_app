1bcf61bd79d699c9a12ee1796fc5b8d75365c5a7
bahromjon begmuhammadov <hopebegmuhammadov@gmail.com>
2021-07-25 15:09:36 +0500

    quick fix

--- apps/main/api/document.py
+++ apps/main/api/document.py
@@ -5,10 +5,5 @@ from main.serializers.document import DocumentSerializer
 
 
 class DocumentViewSet(ModelViewSet):
-    serializer_class = DocumentSerializer
     queryset = Document.objects.all()
-
-    def get_serializer_class(self):
-        context = super(DocumentViewSet, self).get_serializer_context()
-        context.update({"request": self.request})
-        return context
+    serializer_class = DocumentSerializer
