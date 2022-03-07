6e69613dcc1b2301c8e9be27c3e665ec5dca5dd3
bakahtiyor.b@jafton.com <bahtee1997>
2022-03-07 13:19:26 +0500

    migrate bash script is added

--- migrate.sh
+++ migrate.sh
@@ -1,5 +1,5 @@
+#!/bin/bash
 SUPERUSER_EMAIL=${DJANGO_SUPERUSER_EMAIL:-'admin@gmail.com'}
-cd /soil_app
-source venv/bin/activate
-python manage.py migrate --noinput
-python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
+SUPERUSER_PASSWORD=${DJANGO_SUPERUSER_PASSWORD:-'superseckretpassword'}
+/opt/venv/bin/python manage.py migrate --noinput
+/opt/venv/bin/python manage.py createsuperuser --email $SUPERUSER_EMAIL --noinput || true
